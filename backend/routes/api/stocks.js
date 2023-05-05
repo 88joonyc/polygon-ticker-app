00c878e8d93f890a5d287ffd4babe4ec603f51cb
88joonyc <joonyc88@gmail.com>
2023-05-05 17:31:05 -0400

    stocks

--- backend/routes/api/stocks.js
+++ backend/routes/api/stocks.js
@@ -2,3 +2,17 @@ const express = require('express')
 const router = express.Router();
 const asyncHandler = require('express-async-handler');
 const { check } = require('express-validator');
+
+const {  Stock } = require('../../db/models');
+
+router.get('/:id', asyncHandler(async (req, res) => {
+        const { id } = req.params;
+        
+        const stocks = await Stock.all({ id })
+
+        return res.json({
+            stocks
+        })
+
+    })
+);
