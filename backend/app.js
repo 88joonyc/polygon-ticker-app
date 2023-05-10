7137da4ed960e70ac6b71ab268da339516937fe2
88joonyc <joonyc88@gmail.com>
2023-05-10 19:11:57 -0400

    react_app

--- backend/app.js
+++ backend/app.js
@@ -69,20 +69,24 @@ app.use((err, _req, res, _next) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 app.get("*", function (_, res) {
   res.sendFile(
-    path.join(__dirname, "./client/build/index.html"),
+    path.join(__dirname, "client/build, 'index.html"),
     function (err) {
       res.status(500).send(err);
     }
   );
 });
 
-// const PORT = process.env.PORT || 5000
+if (isProduction) {
+    app.get('/', function(req, res) {
+        res.render('index.html', { REACT_APP_API_URL });
+    });
+};
 
-// app.listen(PORT, () => console.log(`listening on port:${PORT}`))
+const PORT = process.env.PORT || 5000
 
-
+app.listen(PORT, () => console.log(`listening on port:${PORT}`))
 
 module.exports = app;
