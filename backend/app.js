3b42f8592bcae0fd7f9e0ccf964ccd5f294b72aa
88joonyc <joonyc88@gmail.com>
2023-05-09 23:22:25 -0400

    port

--- backend/app.js
+++ backend/app.js
@@ -79,9 +79,9 @@ app.get("*", function (_, res) {
   );
 });
 
-const PORT = process.env.PORT || 5000
+// const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`listening on port:${PORT}`))
+// app.listen(PORT, () => console.log(`listening on port:${PORT}`))
 
 
 
