662462282e45295a4106b8731e5e66ca2ee6e3ce
88joonyc <joonyc88@gmail.com>
2023-06-15 11:27:39 -0400

    working

--- frontend/src/store/multiple.js
+++ frontend/src/store/multiple.js
@@ -1,7 +1,5 @@
 import { csrfFetch } from "./csrf";
 
-const initialState = { multiple: null };
-
 const SET_MULTIPLE = 'multiple/setMultiple';
 
 const setMultiple = (multiple) => {
@@ -11,34 +9,36 @@ const setMultiple = (multiple) => {
     }
 };
 
-export const fetchMultipleTickers = (stocksData, dayBefore, dayCounter) => async (dispatch) => {
-        console.log('thisisrunning')
-        let response
-        try{
-            response = await csrfFetch('/api/ticker/search/multiple', {
-                method: 'POST',
-                'Content-type': 'application/JSON',
-                body: JSON.stringify({
-                    symbols: stocksData?.map(stock => stock?.ticker),
-                    to: dayBefore,
-                    from: dayCounter,
-                })
+export const fetchMultipleTickers = ({stocksData, dayBefore, dayCounter}) => async (dispatch) => {
+    let response
+    try{
+        response = await csrfFetch('/api/ticker/search/multiple', {
+            method: 'POST',
+            'Content-type': 'application/JSON',
+            body: JSON.stringify({
+                symbols: stocksData?.map(stock => stock?.ticker),
+                to: dayBefore,
+                from: dayCounter,
             })
-        } catch(err) {
-            console.log(err)
-        }
-        const data = await response.json()
-        // setData(data)
+        })
+    } catch(err) {
+        console.log(err)
+    }
+    const data = await response.json()
+ 
+    dispatch(setMultiple(data))
 
-        return data
+    return data
 };
 
-const multipleReducer = function(state = initialState, action){
+const initialState = { multiple: null };
+
+const multipleReducer = (state = initialState, action) => {
     let newState;
     switch(action.type) {
         case SET_MULTIPLE:
             newState = Object.assign({}, state);
-            newState.multiple = null;
+            newState.multiple = action.payload;
             return newState
         default:
             return state;
