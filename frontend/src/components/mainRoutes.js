c6b60fd9f1dcf3766c67a120220c12015d63f2a4
88joonyc <joonyc88@gmail.com>
2023-06-29 16:07:23 -0400

    mainroutes

--- frontend/src/components/mainRoutes.js
+++ frontend/src/components/mainRoutes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route,  Routes  } from 'react-router-dom'
 
 import TickerForm from './tickerForm';
@@ -13,18 +13,20 @@ import SignupPage from '../pages/SignupPage'
 import LoginPage from '../pages/LoginPage'
 
 
-export default function MainRoutes ({isLoaded}) {
+export default function MainRoutes ({isLoaded, showMenu, setShowMenu, stocksData, total, current, list, investingPriceRef})  {
+
+    const [openWallet, setOpenWallet] = useState(false); 
 
     const session = useSelector(state => state.session.user);
 
     return (    
         <>      
-        <NavBar />
+        <NavBar showMenu={showMenu} setShowMenu={setShowMenu} total={total} investingPriceRef={investingPriceRef}/>
         <Routes>
-            <Route  path='/' element={<> <Home isLoaded={isLoaded} /></>} />
+            <Route  path='/' element={<> <Home openWallet={openWallet} setOpenWallet={setOpenWallet} isLoaded={isLoaded} stocksData={stocksData} total={total} current={current} list={list} investingPriceRef={investingPriceRef}/></>} />
             {/* <Route  path='/ticker/:ticker' element={<Ticker /> } /> */}
         </Routes>
-        <NavBarMobile/>
+        <NavBarMobile  openWallet={openWallet} setOpenWallet={setOpenWallet} showMenu={showMenu} setShowMenu={setShowMenu} />
         </>
 
     )
